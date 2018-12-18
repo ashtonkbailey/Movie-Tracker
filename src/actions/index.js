67eba5da87c5f4c8ab0778cdd54c24f3d0cc8f30
Sara Ebbers <johndoe@example.com>
2018-12-17 19:36:09 -0700

    Comment out actionCreators until ready for use

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,12 +8,12 @@ export const addMovies = (movies) => ({
 // user actions
 //////// add favorite, delete favorite, sign in, sign out, create user
 
-export const addUser = (userObject) => ({
-  type: 'ADD_USER',
-  user
-})
+// export const addUser = (userObject) => ({
+//   type: 'ADD_USER',
+//   user
+// })
 
-export const logInUser = (userObject) => ({
-  type: 'LOG_IN_USER',
-  user
-})
+// export const logInUser = (userObject) => ({
+//   type: 'LOG_IN_USER',
+//   user
+// })
