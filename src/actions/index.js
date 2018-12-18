e79878ad42dbb4014ece82ef7b856a7e5c36b873
Ashton Bailey <ashtonkbailey@gmail.com>
2018-12-18 16:40:11 -0700

    Add switch, routes to app

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,23 @@
 /////// add movies
 export const addMovies = (movies) => ({
   type: 'ADD_MOVIES',
-  movies
-})
+  movies,
+});
 
 // user actions
 //////// add favorite, delete favorite, sign in, sign out, create user
 
 export const addUser = (user) => ({
   type: 'ADD_USER',
-  user
-})
+  user,
+});
 
 export const logInUser = (user) => ({
   type: 'LOG_IN_USER',
-  user
-})
+  user,
+});
+
+export const logOutUser = (user) => ({
+  type: 'LOG_OUT_USER',
+  user,
+});
