1168a36c0f786fa67ba329d3ede177fa1803e33d
Sara Ebbers <johndoe@example.com>
2018-12-20 15:40:09 -0700

    Add api calls for favorites

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,36 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import '../../index.scss';
+import { addFavorite } from '../../utils/apiCalls'
 
 
-const Movie = ({ title, id, rating, text, release, poster, user }) => {
-  let button
-  if (!user.name) {
-    button = (
-      <Link to='/login'>
-        <button className="add-to-favs">Login to Save</button>
-      </Link>
-    )
-  } else {
-    button = <button className="add-to-favs">Add to Favorites</button>
+class Movie extends Component {
+  
+  handleAddFavorite = async () => {
+    const { title, id, rating, text, release, poster, user } = this.props;
+
+    let favoriteObj = {
+      title: title,
+      movie_id: id,
+      vote_average: rating,
+      overview: text,
+      release_date: release,
+      poster_path: poster,
+      user_id: user.id
+    }
+    try {
+      await addFavorite(favoriteObj)
+    } catch(error) {
+      console.log(error)
+    }
+    
   }
 
-  return(
-    <div className="movie">
-      <img src={`https://image.tmdb.org/t/p/w500${poster}`} alt='' />
-      <div className="movie-info">
-        <div className="rating">
-          <span>{rating.toFixed(1)}</span>
-          <i className="fas fa-star" />
-          </div>
-        <h2>{title}</h2>
-        <h5>{release}</h5>
-        <p>{text}</p>
-        {button}
+
+  render() {
+    const { title, id, rating, text, release, poster, user } = this.props;
+
+    let button
+    if (!user.name) {
+      button = (
+        <Link to='/login'>
+          <button className="add-to-favs">Login to Save</button>
+        </Link>
+      )
+    } else {
+      button = <button onClick={()=>this.handleAddFavorite()}className="add-to-favs">Add to Favorites</button>
+    }
+
+    return(
+      <div className="movie">
+        <img src={`https://image.tmdb.org/t/p/w500${poster}`} alt='' />
+        <div className="movie-info">
+          <div className="rating">
+            <span>{rating.toFixed(1)}</span>
+            <i className="fas fa-star" />
+            </div>
+          <h2>{title}</h2>
+          <h5>{release}</h5>
+          <p>{text}</p>
+          {button}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
