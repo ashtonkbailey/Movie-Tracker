e9ad5c6a732210857886907e31ab0d117639bdd4
Jamie Rushford <jar.rushford@gmail.com>
2018-12-28 10:34:50 -0500

    dispatch test

--- src/containers/Movie/Movie.test.js
+++ src/containers/Movie/Movie.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Movie, { mapStateToProps, mapDispatchToProps }from './Movie';
+import Movie, { mapStateToProps, mapDispatchToProps } from './Movie';
 import { shallow } from 'enzyme'; 
 import { addFavoriteThunk } from '../../thunks/addFavorite';
 import { getFavoritesThunk } from '../../thunks/getFavorites';
@@ -8,7 +7,6 @@ import { removeFavoriteThunk } from '../../thunks/removeFavorite';
 
 
 describe('Movie', () => {
-
   describe('Movie Component', () => {
     it('should match the snapshot when there is not a user loged in', () => {
       const mockUser = {name: ''}
@@ -35,29 +33,31 @@ describe('Movie', () => {
       expect(wrapper).toMatchSnapshot()
     })
   })
-  // describe('mapStateToProps', () => {
-  //   it('should parse the user and favorites from state', () => {
-  //     let mockUser = {name: ''}
-  //     let mockFav = {
-  //       title: 'MovieTitle',
-  //       id: 123,
-  //       rating: 9,
-  //       text: 'MovieText',
-  //       release: 'May4bewithyou'
-  //     }
-  //     let mockFavoriteArray = [mockFav]
-  //     let mockState = {
-  //       favorites: mockFavoriteArray,
-  //       user: mockUser,
-  //       other: 'other thing in state'
-  //     }
-  //     const expectedFavorite = mockFavoriteArray
-  //     const expectedUser = mockUser
-  //     const mappedProps = mapStateToProps(mockState)
-  //     expect(mappedProps.favorites).toEqual(expectedFavorite)
-  //     expect(mappedProps.user).toEqual(expectedUser)
-  //   })
-  // })
+
+  describe('mapStateToProps', () => {
+    it('should parse the user and favorites from state', () => {
+      let mockUser = {name: ''}
+      let mockFav = {
+        title: 'MovieTitle',
+        id: 123,
+        rating: 9,
+        text: 'MovieText',
+        release: 'May4bewithyou'
+      }
+      let mockFavoriteArray = [mockFav]
+      let mockState = {
+        favorites: mockFavoriteArray,
+        user: mockUser,
+        other: 'other thing in state'
+      }
+      const expectedFavorite = mockFavoriteArray
+      const expectedUser = mockUser
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps.favorites).toEqual(expectedFavorite)
+      expect(mappedProps.user).toEqual(expectedUser)
+    })
+  })
+
   describe('mapDispatchToProps', () => {
 
     it('should return a props object with the method addFavoriteThunk', () => {
@@ -67,15 +67,16 @@ describe('Movie', () => {
         rating: 9,
         text: 'MovieText',
         release: 'Maythe4thbewithyou'
-      }
+      } 
       const favorites = []
       const mockDispatch = jest.fn()
-      const action = addFavoriteThunk(favoriteObj, favorites)
 
       const mappedProps = mapDispatchToProps(mockDispatch)
       mappedProps.addFavoriteThunk(favoriteObj, favorites)
+      mappedProps.getFavoritesThunk(2)
+      mappedProps.removeFavoriteThunk(123, 2)
 
-      expect(mockDispatch).toHaveBeenCalledWith(action)
+      expect(mockDispatch).toBeCalledTimes(3)
     })
   })
 })
