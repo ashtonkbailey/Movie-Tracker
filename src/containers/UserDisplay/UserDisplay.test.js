bbb298377e1ad3495b1139c30657498dfedd4095
Jamie Rushford <jar.rushford@gmail.com>
2018-12-31 10:44:10 -0700

    Add UserDisplay and reducer tests

--- src/containers/UserDisplay/UserDisplay.test.js
+++ src/containers/UserDisplay/UserDisplay.test.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import UserDisplay from './UserDisplay';
 import { shallow } from 'enzyme';
+import { mapStateToProps, mapDispatchToProps, UserDisplay } from './UserDisplay'
+import { logOutUser } from '../../actions/index';
 
 describe('UserDisplay', () => {
+  let mockUser
+
+  beforeEach(() => {
+    mockUser = { name: 'Bob', email: 'bob@gmail.com', password: 'bob' }
+  })
 
   describe('UserDisplay Component', () => {
     it('should match the snapshot', () => {
-      const wrapper = shallow(<UserDisplay />)
+      const wrapper = shallow(<UserDisplay user={mockUser} />)
       expect(wrapper).toMatchSnapshot()
     })
 
+    it('should match the snapshot with no user', () => {
+      const noUser = { name: '', email: '', password: '' }
+      const wrapper = shallow(<UserDisplay user={noUser} />)
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return a props object with data from store', () => {
+      const mockState = { user: mockUser }
+      const mappedProps = mapStateToProps(mockState)
+
+      expect(mappedProps).toEqual(mockState)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should return a props object with a method logOutUser', () => {
+      const mockDispatch = jest.fn()
+      const expected = logOutUser()
+
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      mappedProps.logOutUser()
+
+      expect(mockDispatch).toBeCalledWith(expected)
+    })
   })
 })
