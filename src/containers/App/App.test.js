2c606d00a227e9ef9349ca0ac21d38269dfef33d
Jamie Rushford <jar.rushford@gmail.com>
2018-12-31 10:26:45 -0700

    Add App Tests

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { shallow } from 'enzyme';
+import { shallow , mount} from 'enzyme';
+import { mapStateToProps, mapDispatchToProps, App } from '../App/App';
+import * as clean from '../../utils/cleaner'
+import * as actions from '../../actions/index';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { MemoryRouter } from 'react-router-dom'
+
+jest.mock('../../utils/cleaner')
 
 describe('App', () => {
 
@@ -11,5 +17,40 @@ describe('App', () => {
       expect(wrapper).toMatchSnapshot()
     })
 
+    it('should get movies when the component mounts', async () => {
+      const mockAdd = jest.fn()
+      const wrapper = shallow(<App addMovies={mockAdd} />)
+
+      await wrapper.instance().componentDidMount()
+
+      expect(mockAdd).toBeCalled()
+    }) 
+  })
+
+  describe('routes', () => {
+    /// Can add routes if we want to / have time but not needed if we dont
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return a props object with data from store', () => {
+      const mockState = {
+        movies: [{ title: 'Movie', rating: 4, description: 'stuff'}]
+      }
+      const mappedProps = mapStateToProps(mockState)
+
+      expect(mappedProps).toEqual(mockState)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should return a props object with a method addMovies', () => {
+      const mockDispatch = jest.fn()
+      const expected = actions.addMovies()
+
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      mappedProps.addMovies()
+
+      expect(mockDispatch).toBeCalledWith(expected)
+    })
   })
 })
